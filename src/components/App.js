d19e22dc6904342c5cef198b0fc293c2d831b329
annet-mwangi <annettestephensa01@gmail.com>
2024-08-19 07:03:17 -0700

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import posts from "../data/posts"; // Ensure you have a data source for posts
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="Underreacted" />
+      <About about="A blog about React" />
+      <main>
+        <ArticleList posts={posts} />
+      </main>
     </div>
   );
 }
